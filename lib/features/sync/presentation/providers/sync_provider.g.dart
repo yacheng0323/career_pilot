d5362c8d2e54f17bb4a91311b2f8a9638f9cbd69
yacheng0323 <yacheng0323@email.com>
2026-06-04 21:20:35 +0800

    feat: add SyncNotifier and sync button to JobListScreen

---
+++ lib/features/sync/presentation/providers/sync_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sync_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$syncNotifierHash() => r'0bb65619263e1532d78a7dd23355555c6f92d52c';
+
+/// See also [SyncNotifier].
+@ProviderFor(SyncNotifier)
+final syncNotifierProvider =
+    AutoDisposeNotifierProvider<SyncNotifier, SyncState>.internal(
+      SyncNotifier.new,
+      name: r'syncNotifierProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$syncNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$SyncNotifier = AutoDisposeNotifier<SyncState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
