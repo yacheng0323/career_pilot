fc8dd4e1f9cdf935a39f6536b03c4cea528aca6a
yacheng0323 <yacheng0323@email.com>
2026-06-05 11:30:52 +0800

    feat(m5a): paginated ExploreScreen with infinite scroll and pull-to-refresh

---
+++ lib/features/jobs/presentation/providers/job_list_paginated_provider.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'job_list_paginated_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$jobListPaginatedHash() => r'960c536822fb19661041800d0a79032ac4d8d8b3';
+
+/// See also [JobListPaginated].
+@ProviderFor(JobListPaginated)
+final jobListPaginatedProvider =
+    AutoDisposeNotifierProvider<
+      JobListPaginated,
+      AsyncValue<List<Job>>
+    >.internal(
+      JobListPaginated.new,
+      name: r'jobListPaginatedProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$jobListPaginatedHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$JobListPaginated = AutoDisposeNotifier<AsyncValue<List<Job>>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
